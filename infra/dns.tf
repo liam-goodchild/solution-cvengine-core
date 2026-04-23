588db7cae04f751c3cdd6e684be128f372bb1a06
Liam Goodchild <liamgoodchild12@hotmail.co.uk>
2026-04-23 23:10:23 +0100

    Adjust Cloudflare DNS record settings

--- infra/dns.tf
+++ infra/dns.tf
@@ -3,14 +3,6 @@ resource "cloudflare_dns_record" "swa_cname" {
   type    = "CNAME"
   name    = var.dns_zone_name
   content = azurerm_static_web_app.main.default_host_name
-  proxied = true
+  proxied = false
   ttl     = 1
-}
-
-resource "cloudflare_dns_record" "swa_txt" {
-  zone_id = local.cloudflare_zone_id
-  type    = "TXT"
-  name    = var.dns_zone_name
-  content = azurerm_static_web_app_custom_domain.main.validation_token
-  ttl     = 3600
 }
